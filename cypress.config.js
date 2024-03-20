da3ba4996a060fbf098a788f19b122bd9071e234
X415795 <mamrabet@vermeg.com>
2024-03-20 02:04:45 +0100

    test

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: 'xg7544',
+  fixturesFolder: 'cypress/fixtures',
+  screenshotsFolder: 'cypress/screenshots',
+  viewportWidth: 1600,
+  viewportHeight: 760,
+  video: true,
+  videosFolder: "cypress/videos", // The folder where recorded videos will be stored
+  chromeWebSecurity: false,
+  experimentalSourceRewriting: true,
+  e2e: {
+    supportFile:false,
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://10.1.146.29:8080/#/',
+  },
+})
