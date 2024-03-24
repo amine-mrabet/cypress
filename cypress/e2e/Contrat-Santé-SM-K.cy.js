2b5d97084d597df5d5628da19458955b0f187ca2
X415795 <mamrabet@vermeg.com>
2024-03-24 02:33:39 +0100

    "Update tests for creating Saisie and Project; add dynamically generated data for demography section"
    This commit updates the tests for creating a Saisie and a Project in the web application. It includes logging in, navigating to the appropriate pages, and entering required information. The commit also adds a function to add dynamically generated data for the demography section of the Project.
    
    The changes include:
    
    * Updating the login credentials for the B300NQA user
    * Adding a check to ensure that the login is successful before proceeding
    * Adding a function to create a Saisie, including adding an établissement, apporteur, and project description
    * Adding a function to create a Project, including assigning the Project and opening it
    * Adding a function to add dynamically generated data for the demography section of the Project
    
    The changes also include updates to the data used for the établissement and project description, as well as updates to the selectors used to interact with the application.

--- cypress/e2e/Contrat-Santé-SM-K.cy.js
+++ cypress/e2e/Contrat-Santé-SM-K.cy.js
@@ -3,65 +3,89 @@ const b300nqa = require('./profile/b300nqa.json');
 const ETABLISSEMENT = require('./data-json/Etablissement.json');
 const Apporteur = require('./data-json/Apporteur-interlocuteur-inspecteur.json')
 const DESCPROJECT = require('./data-json/project-description.json')
+const DEMOGRAPHY  = require('./data-json/demography.json')
 const FILE = require('./data-json/file.json')
 // Generate a random 14-digit number
 const randomNumber14 = Math.floor(10000000000000 + Math.random() * 90000000000000);
 describe('My Web Application Tests', () => {
-  it('create Saisie', () => {
+    it('create Saisie', () => {
+      cy.visit('/');
+      Login(b012cag.username, b012cag.password)
+      Cypress.on('uncaught:exception', (err, runnable) => {
+        return false;
+      });
+      //
+      const isLoginSuccessful = checkLogin(b012cag.username, b012cag.password, b012cag);
+      if (isLoginSuccessful) {
+        cy.visit('/');
+        cy.wait(3000);
+        cy.get('.create-request button').should('exist').click();
+        addEtablisement();
+        cy.get('#scrollTopBtn').should('exist').click();
+        cy.get('.checkmark-container input[type=checkbox]').first().should('exist').click({ force: true });
+        cy.get('.broker-inspector-info a').should('exist').click();
+        cy.wait(1000);
+        addApporteur()
+        cy.get('.project-description a').should('exist').click();
+        cy.wait(1000);
+        projectDescription()
+        cy.get('.Synthese-saisie a').should('exist').click();
+        cy.wait(1000);
+        cy.get('app-validation-sasie .style-save button').should('exist').click();
+        cy.wait(15000).then(() => {
+          cy.get('.validation-saisie a').should('exist').click();
+        })
+        cy.get(".request-reason").should('exist').click();
+        cy.get(".p-dropdown-filter").should('exist').type("Conquête")
+        cy.get('p-dropdownitem').contains("Conquête").should('exist').click();
+        cy.wait(3000).then(() => {
+          cy.get('app-synthese-saisie .create-project button').should('exist').click();
+        })
+        cy.wait(7000);
+      } else {
+        console.log('Login failed');
+      }
+    });
+
+  it('create Project', () => {
+    //login
     cy.visit('/');
-    Login(b012cag.username, b012cag.password)
+    Login(b300nqa.username, b300nqa.password)
     Cypress.on('uncaught:exception', (err, runnable) => {
       return false;
     });
     //
-    const isLoginSuccessful = checkLogin(b012cag.username, b012cag.password, b012cag);
+    const isLoginSuccessful = checkLogin(b300nqa.username, b300nqa.password, b300nqa);
     if (isLoginSuccessful) {
       cy.visit('/');
-      cy.wait(3000);
-      cy.get('.create-request button').should('exist').click();
-      addEtablisement();
-      cy.get('#scrollTopBtn').should('exist').click();
-      cy.get('.checkmark-container input[type=checkbox]').first().should('exist').click({ force: true });
-      cy.get('.broker-inspector-info a').should('exist').click();
-      cy.wait(1000);
-      addApporteur()
-      cy.get('.project-description a').should('exist').click();
-      cy.wait(1000);
-      projectDescription()
-      cy.get('.Synthese-saisie a').should('exist').click();
-      cy.wait(1000);
-      cy.get('app-validation-sasie .style-save button').should('exist').click();
-      cy.wait(8000).then(() => {
-        cy.get('.validation-saisie a').should('exist').click();
+      cy.wait(7000).then(() => {
+        cy.get('.search-tabs ul li').eq(1).click();
+        cy.get('.project-0 .button-expandable-project button').should('exist').click();
+        cy.get('.row-expanded .vp-col-actions .plm-grouped-button button').should('exist').click();
+        cy.get('.assigner-action button').should('exist').click();
+        cy.wait(3000);
+        cy.get('.row-expanded .vp-col-actions .plm-grouped-button button').should('exist').click();
+        cy.get(' .ouvrir-action button').should('exist').click();
+        cy.wait(3000).then(() => {
+           cy.wait(3000);
+          cy.contains('li span', 'Contexte du projet').click();
+          cy.get('.risk-sante button').should('exist').click();
+          cy.contains('.p-datepicker-buttonbar button span', "Aujourd'hui").click();
+          cy.get('#scrollTopBtn').should('exist').click();
+          cy.wait(1000);
+          cy.get('.suivant-action button').should('exist').click({waitForAnimations: false});
+          cy.wait(13000);
+          addDynamiqueData(DEMOGRAPHY)
+          cy.get('.valider-demographie button').should('exist').click({waitForAnimations: false});
+  
+        })
       })
-      cy.get(".request-reason").should('exist').click();
-      cy.get(".p-dropdown-filter").should('exist').type("Conquête")
-      cy.get('p-dropdownitem').contains("Conquête").should('exist').click();
 
- 
 
     } else {
       console.log('Login failed');
     }
   });
-
-  /*     it('create Project', () => {
-        //login
-        cy.visit('/');
-        cy.wait(2000);
-        Login(b300nqa.username,b300nqa.password)
-        cy.wait(1000);
-        Cypress.on('uncaught:exception', (err, runnable) => {
-          return false;
-        });
-        //
-        const isLoginSuccessful = checkLogin(b300nqa.username, b300nqa.password,b300nqa);
-        if (isLoginSuccessful) {
-          cy.visit('/');
-          } else {
-          console.log('Login failed');
-        }
-      }); */
 
 });
 /**
@@ -99,12 +123,12 @@ function addEtablisement() {
       }
       if (item[key].type == 'input-text') {
         cy.get(item[key].selectorAttr).should('exist').clear({ force: true });
-        if(item[key].random){
+        if (item[key].random) {
           cy.get(item[key].selectorAttr).should('exist').type(randomNumber14.toString(), { force: true });
-        }else{
+        } else {
           cy.get(item[key].selectorAttr).should('exist').type(item[key].value, { force: true });
         }
-        
+
       }
       if (item[key].type == 'dropDown') {
         cy.get(item[key].selectorAttr).should('exist').click();
@@ -163,6 +187,34 @@ function projectDescription() {
   uploadFile("#fileDropRef", FILE.filecontent, "exemple.xls")
 
 
+}
+/**
+ * 
+ * @param {*} data 
+ */
+function addDynamiqueData(data) {
+  Object.entries(data).forEach(([key, value]) => {
+    if (data[key].type == 'checkbox') {
+      cy.get(data[key].selectorAttr).click({ force: true });
+    }
+    if (data[key].type == 'input-text') {
+      cy.get(data[key].selectorAttr).should('exist').clear({ force: true });
+      if (data[key].random) {
+        cy.get(data[key].selectorAttr).should('exist').type(randomNumber14.toString(), { force: true });
+      } else {
+        cy.get(data[key].selectorAttr).should('exist').type(data[key].value, { force: true });
+      }
+    }
+    if (data[key].type == 'dropDown') {
+      cy.get(data[key].selectorAttr).should('exist').click();
+      cy.get(".p-dropdown-filter").should('exist').type(data[key].labelFilter)
+      cy.get('p-dropdownitem').contains(data[key].labelFilter).should('exist').click();
+    }
+    if (data[key].type == 'dropDownNoFilter') {
+      cy.get(data[key].selectorAttr).should('exist').click();
+      cy.get('p-dropdownitem').contains(data[key].labelFilter).should('exist').click();
+    }
+  })
 }
 /**
  * 
